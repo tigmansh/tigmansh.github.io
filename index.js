cf4d7970d8ae154bed07123b4560875b1c6f4ae4
tigmansh <gargtigmanshu@gmail.com>
2023-03-24 17:18:37 +0530

    Contact section done with google app scripts

--- index.js
+++ index.js
@@ -1,118 +1,112 @@
-document.getElementById("resume-link-1").addEventListener("click",()=>{
-window.open("https://drive.google.com/file/d/1WpgDFD5117vI0FJqaOSq8Amw-Cx8R5sd/view?usp=share_link");
-})
+document.getElementById("resume-link-1").addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1WpgDFD5117vI0FJqaOSq8Amw-Cx8R5sd/view?usp=share_link"
+  );
+});
 
-document.getElementById("resume-link-2").addEventListener("click",()=>{
-    window.open("https://drive.google.com/file/d/1WpgDFD5117vI0FJqaOSq8Amw-Cx8R5sd/view?usp=share_link");
-})
+document.getElementById("resume-link-2").addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1WpgDFD5117vI0FJqaOSq8Amw-Cx8R5sd/view?usp=share_link"
+  );
+});
 
+let details = document.getElementsByClassName("skill-ed");
+let content = document.getElementsByClassName("contents");
 
-let details=document.getElementsByClassName("skill-ed");
-let content=document.getElementsByClassName("contents");
+opentab = (name) => {
+  for (detail of details) {
+    detail.classList.remove("active");
+  }
+  for (con of content) {
+    con.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active");
+  document.getElementById(name).classList.add("active-tab");
+};
 
-opentab=(name)=>{
-    for(detail of details){
-        detail.classList.remove("active");
-    }
-    for(con of content){
-        con.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active");
-    document.getElementById(name).classList.add("active-tab");
-}
+let sidemenu = document.getElementById("sidemenu");
 
+openmenu = () => {
+  sidemenu.style.right = "0";
+};
 
-let sidemenu=document.getElementById("sidemenu");
+closemenu = () => {
+  sidemenu.style.right = "-200px";
+};
 
-openmenu=()=>{
-sidemenu.style.right="0";
-}
-
-closemenu=()=>{
-    sidemenu.style.right="-200px";
-}
-
-
-
-
-
-
-  const scriptURL = "https://script.google.com/macros/s/AKfycbxikQI-lHaHmDgM8jOrq1aXC-9aLdMrGUPqWZeuw1GshvdLAZfz294EJZxsi0MuG3_H/exec"
-  const form = document.forms['submit-to-google-sheet']
-  const msg=document.getElementById("msg");
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML="Message Sent Successfully";
-        setTimeout(()=>{
-            msg.innerHTML="";
-        },5000);
-        form.reset();
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbx82hI_piwkiQD_at1DeSyXlBLq53Twggg1suj3j8zfUQryozddn4zZHOQb1bYPbQfs/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message Sent Successfully";
+      setTimeout(() => {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
     })
-      .catch(error => console.error('Error!', error.message))
-  })
+    .catch((error) => console.error("Error!", error.message));
+});
 
-window.addEventListener("scroll",function(){
-    let nav=this.document.querySelector("nav");
-    nav.classList.toggle("sticky",window.scrollY > 0);
-})
- 
-let container=  document.getElementById("skill_container");
- 
-let skill_arr=[
-    
-    "./images/html.png",
-    "./images/css.png",
-    "./images/js.png",
-    "./images/nodejslogo.png",
-    "./images/express.png",
-    "./images/mongoose.png",
-    "./images/mongodb.png",
-    "./images/sql.jpg",
-    "./images/redis.png",
-    "./images/aws.png",
-    "./images/git.png",
-    
+window.addEventListener("scroll", function () {
+  let nav = this.document.querySelector("nav");
+  nav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+let container = document.getElementById("skill_container");
+
+let skill_arr = [
+  "./images/html.png",
+  "./images/css.png",
+  "./images/js.png",
+  "./images/nodejslogo.png",
+  "./images/express.png",
+  "./images/mongoose.png",
+  "./images/mongodb.png",
+  "./images/sql.jpg",
+  "./images/redis.png",
+  "./images/aws.png",
+  "./images/git.png",
 ];
-let skill_name=[
-    
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "Node.JS",
-    "EXPRESS.JS",
-    "MONGOOSE",
-    "MONGODB",
-    "SQL",
-    "REDIS",
-    "AWS",
-    "GIT",
-]
-let count=0;
-AppendSkills=(arr)=>{
-container.innerHTML=null;
-    arr.forEach((ele)=>{
-        let div= document.createElement("div");
+let skill_name = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Node.JS",
+  "EXPRESS.JS",
+  "MONGOOSE",
+  "MONGODB",
+  "SQL",
+  "REDIS",
+  "AWS",
+  "GIT",
+];
+let count = 0;
+AppendSkills = (arr) => {
+  container.innerHTML = null;
+  arr.forEach((ele) => {
+    let div = document.createElement("div");
 
-        div.classList.add("skills-card");
+    div.classList.add("skills-card");
 
-        let img=document.createElement("img");
-        img.src=ele;
-        img.classList.add("skills-card-img")
+    let img = document.createElement("img");
+    img.src = ele;
+    img.classList.add("skills-card-img");
 
-        let name=document.createElement("h2");
-        name.classList.add("skills-card-name");
-        
-        name.innerText=skill_name[count];
-        name.style.color="white";
-        count++;
-        div.append(img,name);
-        container.append(div);
-    })
-}
+    let name = document.createElement("h2");
+    name.classList.add("skills-card-name");
 
-window.addEventListener("load",()=>{
-    AppendSkills(skill_arr);
-})
+    name.innerText = skill_name[count];
+    name.style.color = "white";
+    count++;
+    div.append(img, name);
+    container.append(div);
+  });
+};
 
+window.addEventListener("load", () => {
+  AppendSkills(skill_arr);
+});
